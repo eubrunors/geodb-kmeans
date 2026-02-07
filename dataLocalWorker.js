97ad816f642692d78341513fb64e9cade0e57660
eubrunors <bruno.2013.br11@gmail.com>
2026-02-06 23:44:34 -0300

    refactor(dataLocalWorker): simplificar processamento de chunks
    - Remove comentários antigos
    - Mantém apenas filtragem e mapeamento de campos válidos
    - Prepara dados para SharedArrayBuffer

--- dataLocalWorker.js
+++ dataLocalWorker.js
@@ -1,11 +1,5 @@
 self.onmessage = (e) => {
   const { chunk } = e.data;
-  
-  // Aqui você pode:
-  // - validar campos
-  // - normalizar estrutura
-  // - filtrar registros inválidos
-
   const processed = chunk
     .filter(c => c.latitude && c.longitude && c.population)
     .map(c => ({
